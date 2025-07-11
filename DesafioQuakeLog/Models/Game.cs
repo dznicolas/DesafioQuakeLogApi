8d3870d903a997068657fd190269224b068a6b48
Nicolas - Dev <nicolasdzsilva@gmail.com>
2025-07-11 11:40:15 -0300

    Add Models for the API project

---
+++ DesafioQuakeLog/Models/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace DesafioQuakeLog.Models;
+
+public class Game
+{
+    public string Name { get; set; }
+    public int TotalKills { get; set; }
+    public List<string> Players { get; set; } = [];
+    public Dictionary<string, int> Kills { get; set; } = [];
+}
